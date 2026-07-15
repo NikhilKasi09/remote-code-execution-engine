72cbd44cc37b601a98e4c334acdacc7f0985fb15
nk1925 <nk1925@ic.ac.uk>
2026-07-15 12:40:14 +0100

    Added a fake frontend test

--- backend/execution-engine/src/main/java/com/rce/execution_engine/ExecutionEngineApplication.java
+++ backend/execution-engine/src/main/java/com/rce/execution_engine/ExecutionEngineApplication.java
@@ -1,7 +1,9 @@
 package com.rce.execution_engine;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExecutionEngineApplication {
@@ -10,4 +12,23 @@ public class ExecutionEngineApplication {
 		SpringApplication.run(ExecutionEngineApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runTest(CodeExecutionService executionService) {
+		return args -> {
+			System.out.println("--- STARTING PHASE 1 TEST ---");
+
+			// The hardcoded Python script we want to test
+			String pythonCode = "print('Hello from the Docker Sandbox in Cardiff!')\n" +
+					"x = 10\n" +
+					"y = 20\n" +
+					"print(f'The sum is {x + y}')";
+
+			// Trigger the engine
+			String result = executionService.executePython(pythonCode);
+
+			// Print the captured output from the Docker container
+			System.out.println("Execution Result:\n" + result);
+			System.out.println("--- PHASE 1 TEST COMPLETE ---");
+		};
+	}
 }
