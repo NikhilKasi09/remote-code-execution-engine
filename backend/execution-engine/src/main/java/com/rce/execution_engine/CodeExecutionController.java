b38f228986f54f07bb134a3eea4dbdeb0e0f02ca
nk1925 <nk1925@ic.ac.uk>
2026-07-16 17:58:22 +0100

    Created a REST endpoint that accepts and returns JSON

---
+++ backend/execution-engine/src/main/java/com/rce/execution_engine/CodeExecutionController.java
@@ -0,0 +1,46 @@
+package com.rce.execution_engine;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*") // Allows frontend communication
+public class CodeExecutionController {
+
+    private final CodeExecutionService executionService;
+
+    public CodeExecutionController(CodeExecutionService executionService){
+        this.executionService = executionService;
+    }
+
+    @PostMapping("/execute") // Listens for incoming POST requests (client -> server) on localhost/api/execute
+    public ResponseEntity<Map<String, String>> executeCode(@RequestBody ExecutionRequest request){
+
+        // Extract the code from the incoming JSON
+        String userCode = request.getCode();
+        String language = request.getLanguage();
+
+        String output;
+
+        // Execute based on language (only have python currently)
+
+        if (language.equalsIgnoreCase("python")){
+            output = executionService.executePython(userCode);
+        } else {
+            output = "Error: Unsupported language selected.";
+        }
+
+        // Format back to JSON
+        Map<String, String> response = new HashMap<>();
+        response.put("output", output);
+
+        return ResponseEntity.ok(response);
+
+
+    }
+}
